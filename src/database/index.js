6180b176a7219e3c5d0ff797851a2e7fdacaf83f
KewinReisWeb <Kewin0Rou@gmail.com>
2026-08-17 23:51:03 -0300

    fix: remove console.log que expunha credenciais do banco

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,5 @@
 const knexfile = require("../../knexfile");
 
-console.log("=== CONFIG DO KNEX ===");
-console.log(knexfile.development);
-
 const knex = require("knex")(knexfile.development);
 
 module.exports = knex;
